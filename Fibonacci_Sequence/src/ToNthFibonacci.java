56991f6749ec358f75986f13d3a849104d6f9798
Christopher Chin <christoph.chin@gmail.com>
2017-05-10 20:10:29 -0700

    Finished

--- Fibonacci_Sequence/src/ToNthFibonacci.java
+++ Fibonacci_Sequence/src/ToNthFibonacci.java
@@ -2,20 +2,62 @@
 public class ToNthFibonacci {
 
     public static void main(String[] args) {
-        System.out.println(toNthFibonacci(4));
+        System.out.println(toNthFibonacci(12));
+        System.out.println(prlongToNthFibonacci(6));
 
     }
     
-    public static int toNthFibonacci(int n){
-        int previous = 1;
-        int second_Previous = 0;
+    /**
+     * @param n How many fibonacci numbers to print
+     * @return the value of the nth fibonacci 
+     * @author Christopher Chin
+     * 
+     * Recursive method, Helper Function
+     * Works up to n = 93 before becomes too big for size of long 
+     */
+    public static long toNthFibonacci(long n){
         
-        for(int i = 0; i < n; i ++){
-            previous += second_Previous;
-            second_Previous = previous - second_Previous;
+        if (n < 0) {
+            throw new RuntimeException("Not a valid index!");
+        } 
+        
+        return prlongFibonacci(0, 1, n);
+    }
+    
+    /**
+     * @param n How many fibonacci numbers to print
+     * @return the value of the nth fibonacci 
+     * @author Christopher Chin
+     * 
+     * Recursive Function
+     * Works up to n = 93 before becomes too big for size of long 
+     */
+    public static long prlongFibonacci(long currentTerm, long nextTerm, long n){
+        if(n == 0){
+            return nextTerm - currentTerm;
         }
-        
-        return previous + second_Previous;
+        System.out.println(currentTerm);
+        return prlongFibonacci(nextTerm, currentTerm + nextTerm, n-1);
     }
 
+    /**
+     * @param n How many fibonacci numbers to print
+     * @return the value of the nth fibonacci 
+     * @author Christopher Chin
+     * 
+     * Iterative Method     
+     * Works up to n = 93 before becomes too big for size of long 
+     */
+    public static long prlongToNthFibonacci(long n){
+        long currentTerm = 0;
+        long nextTerm = 1;
+
+        for(long i = 0; i < n; i ++){
+            System.out.println(currentTerm);
+            nextTerm += currentTerm;
+            currentTerm = nextTerm - currentTerm;
+        }
+        
+        return nextTerm - currentTerm;
+    }
 }
