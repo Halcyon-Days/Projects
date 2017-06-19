9e705dfb72e2d03c79bfdb30004beeff55381d65
Christopher Chin <christoph.chin@gmail.com>
2017-06-19 02:18:01 -0700

    Added comments

--- FastExpo/src/io/github/halcyon_daze/fast_expo/FastExpo.java
+++ FastExpo/src/io/github/halcyon_daze/fast_expo/FastExpo.java
@@ -2,12 +2,21 @@ package io.github.halcyon_daze.fast_expo;
 
 public class FastExpo {
 
-    public static double getExpo(int base, int power){
+    /**
+     * @param base base value for exponential
+     * @param power power for exponential
+     * @return base^power
+     * 
+     * @author Christopher Chin
+     * @date June 19. 2017
+     * 
+     */
+    public static double getPow(int base, int power){
        if (power == 0) {
            return 1;
        }
        
-       double temp = getExpo(base, power/2);
+       double temp = getPow(base, power/2);
        
        if (power%2 == 0){
            return temp*temp;
