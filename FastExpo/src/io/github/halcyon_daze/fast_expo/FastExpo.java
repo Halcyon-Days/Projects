4075c3d67ee7fd23a9e2254c6ad0cfc935d95c97
Christopher Chin <christoph.chin@gmail.com>
2017-06-21 02:26:41 -0700

    Finished

--- FastExpo/src/io/github/halcyon_daze/fast_expo/FastExpo.java
+++ FastExpo/src/io/github/halcyon_daze/fast_expo/FastExpo.java
@@ -3,6 +3,10 @@ package io.github.halcyon_daze.fast_expo;
 public class FastExpo {
 
     /**
+     * Implements a program that finds the value of an integer raised to a power
+     * 
+     * Helped from http://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
+     * 
      * @param base base value for exponential
      * @param power power for exponential
      * @return base^power
