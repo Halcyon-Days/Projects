5972d668fc26a0b6e8f3bb381092cb22309e7d55
Christopher Chin <christoph.chin@gmail.com>
2017-07-05 18:26:52 -0700

    Added Comments

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -1,6 +1,14 @@
 
 public class FizzBuzz {
+    
+    /**
 
+     * Will print all the numbers on the way to 100, if a multiple of 3 then prints fizz, if a multiple of 5 then prints buzz, if a multiple of 3 and 5 then prints fizzbuzz
+     * 
+     * @author Christopher Chin
+     * @date July 5. 2017
+     * 
+     */
     public static void fizzbuzz() {
         for (int i = 1; i < 101; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
