0199ca5791baa68e852ce52400afab9b235ad272
Christopher Chin <christoph.chin@gmail.com>
2017-08-12 17:09:26 -0700

    Added methods for addition, subtraction, division, multiplication and conjugates

--- Complex/src/io/github/halcyon_daze/complex/Complex.java
+++ Complex/src/io/github/halcyon_daze/complex/Complex.java
@@ -4,23 +4,80 @@ public class Complex {
     private int real;
     private int imaginary;
 
-    Complex(int realCo, int imagCo) {
+    Complex (int realCo, int imagCo) {
         real = realCo;
         imaginary = imagCo;
     }
 
-    Complex(int realCo) {
+    Complex (int realCo) {
         real = realCo;
         imaginary = 0;
     }
 
-    void print() {
+    public String toString() {
         if (imaginary == 0) {
-            System.out.println(real);
+            return String.valueOf(real);
+        } else if (real == 0 && imaginary == 1) {
+            return "i";
+        } else if (real == 0 && imaginary == -1) {
+            return "-i";
         } else if (real == 0) {
-            System.out.println(imaginary + "i");
+            return String.valueOf(imaginary + "i");
+        } else if (imaginary == 1){
+            return String.valueOf(real + " + "  + "i");
+        } else if (imaginary == -1){
+            return String.valueOf(real + " - "  + "i");
+        } else if (imaginary < 0){
+            return String.valueOf(real + " - " + (-imaginary) + "i");
         } else {
-            System.out.println(real + " + " + imaginary + "i");
+            return String.valueOf(real + " + " + imaginary + "i");
         }
+    }
+    
+    public void changeValue(int newRealCo, int newImagCo) {
+        real = newRealCo;
+        imaginary = newImagCo;
+    }
+    
+    public int getReal() {
+        return real;
+    }
+    
+    public int getImaginary() {
+        return imaginary;
+    }
+    
+    public Complex add(Complex operand) {
+        return new Complex(this.getReal() + operand.getReal(), this.getImaginary() + operand.getImaginary());
+    }
+    
+    public Complex sub(Complex operand) {
+        return new Complex(this.getReal() - operand.getReal(), this.getImaginary() - operand.getImaginary());
+    }
+    
+    public Complex multiply(Complex operand) {
+        int a = this.getReal();
+        int b = this.getImaginary();
+        int c = operand.getReal();
+        int d = operand.getImaginary();
+        
+        return new Complex(
+                a*c - b*d,
+                a*d + b*c);
+    }
+    
+    public Complex divide(Complex operand) {
+        int a = this.getReal();
+        int b = this.getImaginary();
+        int c = operand.getReal();
+        int d = operand.getImaginary();
+        
+        return new Complex(
+                (a*c + b*d) / (c*c + d*d),
+                (b*c - a*d) / (c*c + d*d));
+    }
+    
+    public Complex Conjugate() {
+        return new Complex(this.real, -this.imaginary);
     }
 }
--- Complex/src/io/github/halcyon_daze/complex/complexTest.java
+++ Complex/src/io/github/halcyon_daze/complex/complexTest.java
@@ -10,15 +10,112 @@ import org.junit.Test;
 public class complexTest {
 
     @Test
-    public void test() {
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outStream));
+    public void testExpression() {
+        Complex[] testNumbers = new Complex[11];
+        
+        testNumbers[0] = new Complex(0);
+        assertEquals("Print real number 0", "0", testNumbers[0].toString());
 
-        Complex complexNumber = new Complex(0);
-        // complexNumber.print();
-        System.out.println("0");
-        assertEquals("0\n", outStream.toString());
-
+        testNumbers[1] = new Complex(100);
+        assertEquals("Print real number 100", "100", testNumbers[1].toString());
+        
+        testNumbers[2] = new Complex(0, 1);
+        assertEquals("Print complex number i", "i", testNumbers[2].toString());
+        
+        testNumbers[3] = new Complex(0, -1);
+        assertEquals("Print complex number -i", "-i", testNumbers[3].toString());
+        
+        testNumbers[4] = new Complex(0, 5);
+        assertEquals("Print complex number 5i", "5i", testNumbers[4].toString());
+        
+        testNumbers[5] = new Complex(1, 5);
+        assertEquals("Print complex number 1 + 5i", "1 + 5i", testNumbers[5].toString());
+        
+        testNumbers[6] = new Complex(1, -5);
+        assertEquals("Print complex number 1 - 5i", "1 - 5i", testNumbers[6].toString());
+        
+        testNumbers[7] = new Complex(-1, -5);
+        assertEquals("Print complex number -1 - 5i", "-1 - 5i", testNumbers[7].toString());
+        
+        testNumbers[8] = new Complex(-1, 5);
+        assertEquals("Print complex number -1 + 5i", "-1 + 5i", testNumbers[8].toString());
+        
+        testNumbers[9] = new Complex(1, -1);
+        assertEquals("Print complex number 1 - i", "1 - i", testNumbers[9].toString());
+        
+        testNumbers[10] = new Complex(-1, -1);
+        assertEquals("Print complex number 1 + i", "-1 - i", testNumbers[10].toString());
+    }
+    
+    @Test
+    public void testChangingValue() {
+        Complex testNumber1 = new Complex(0);
+        assertEquals("Print real number 0", "0", testNumber1.toString());
+        
+        testNumber1.changeValue(1, 1);
+        assertEquals("Print complex number 1 + i", "1 + i", testNumber1.toString());
     }
 
+    @Test
+    public void testAddition() {
+        Complex[] testNumbers = new Complex[9];
+        
+        testNumbers[0] = new Complex(0);
+        assertEquals(" 0 + 0 = 0", "0", testNumbers[0].add(testNumbers[0]).toString());
+        
+        testNumbers[1] = new Complex(1);
+        assertEquals(" 1 + 1 = 2", "2", testNumbers[1].add(testNumbers[1]).toString());
+        
+        testNumbers[2] = new Complex(0, 1);
+        assertEquals(" i + i = 2i", "2i", testNumbers[2].add(testNumbers[2]).toString());
+        
+        testNumbers[3] = new Complex(0, -1);
+        assertEquals(" -i + -i = -2i", "-2i", testNumbers[3].add(testNumbers[3]).toString());
+        
+        testNumbers[4] = new Complex(1, 1);
+        assertEquals(" (1 + i) + (1 + i) = 2 + 2i", "2 + 2i", testNumbers[4].add(testNumbers[4]).toString());
+        
+        testNumbers[5] = new Complex(1, -1);
+        assertEquals(" (1 - i) + (1 - i) = 2 - 2i", "2 - 2i", testNumbers[5].add(testNumbers[5]).toString());
+        
+        testNumbers[6] = new Complex(-1);
+        assertEquals(" -1 + -1 = -2", "-2", testNumbers[6].add(testNumbers[6]).toString());
+        
+        testNumbers[7] = new Complex(-1, -1);
+        assertEquals(" (-1 - i) + (-1 - i) = -2 - 2i", "-2 - 2i", testNumbers[7].add(testNumbers[7]).toString());
+        
+        testNumbers[8] = new Complex(-1, 1);
+        assertEquals(" (1 + i) + (1 + i) = -2 + 2i", "-2 + 2i", testNumbers[8].add(testNumbers[8]).toString());
+        
+    }
+    
+    @Test
+    public void testSubtraction() {
+        Complex[] testNumbers = new Complex[8];
+        
+        testNumbers[0] = new Complex(0);
+        assertEquals(" 0 - 0 = 0", "0", testNumbers[0].sub(testNumbers[0]).toString());
+        
+        testNumbers[1] = new Complex(1);
+        assertEquals(" 1 - 1 = 2", "0", testNumbers[1].sub(testNumbers[1]).toString());
+        
+        testNumbers[2] = new Complex(0, 1);
+        assertEquals(" i - i = -2i", "0", testNumbers[2].sub(testNumbers[2]).toString());
+        
+        testNumbers[3] = new Complex(0, -1);
+        assertEquals(" -i - -i = 0", "0", testNumbers[3].sub(testNumbers[3]).toString());
+        
+        testNumbers[4] = new Complex(1, 1);
+        assertEquals(" (1 + i) - (1 + i) = 0", "0", testNumbers[4].sub(testNumbers[4]).toString());
+        
+        testNumbers[5] = new Complex(1, -1);
+        assertEquals(" (1 - i) - (1 + i) = 0", "-2i", testNumbers[5].sub(testNumbers[4]).toString());
+        
+        assertEquals("(1 + i) - (1 - i)  = 2i", "2i", testNumbers[4].sub(testNumbers[5]).toString());
+        
+        testNumbers[7] = new Complex(-1, -1);
+        assertEquals(" (-1 - i) - (1 + i) = -2 - 2i", "-2 - 2i", testNumbers[7].sub(testNumbers[4]).toString());
+
+        
+    }
 }
